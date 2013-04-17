29c4bf75178ea8465192d045f534864e3000d411
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-17 14:58:15 -0400

    Add the file for Exercise 3, Extra Credit 3

---
+++ ex3-ec3.py
@@ -0,0 +1,16 @@
+# Exercise 3, Extra Credit 3
+
+print "This example is from the cheatsheet 'A Byte of Python - Operator Precedence, Chapter 5.A Operators and Expressions'"
+print""
+print "What is 2 + 3 * 4?"
+print "2 + 3 * 4 =", 2 + 3 * 4
+print ""
+print "The result is the same as 2 + ( 3 * 4 ), which is", 2 + ( 3 * 4 )
+print "But it is different than ( 2 + 3 ) * 4, which is", ( 2 + 3 ) * 4
+print ""
+print ""
+print "*NOTICE* That the cheatsheet reminds us that redundant parentheses are goofy at best."
+print "For example, who needs to write ( 2 + ( 3 + 4 )), when addition is always of the same precedence?"
+print "The parentheses mean nothing in this example."
+print ""
+print ""
