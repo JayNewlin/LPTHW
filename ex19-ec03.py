8bfeb47fa5e9629cf8ebdd6a9e6a037bfee9a28a
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-22 12:42:32 -0400

    Extra Credit 3 works through Monday

--- ex19-ec03.py
+++ ex19-ec03.py
@@ -1,14 +1,14 @@
 # Written for Exercise 19, Extra Credit 3
-# import math
+import math
 from sys import argv
 script, initial_number_of_people = argv
 number_of_people = int(initial_number_of_people)
 
 def people_per_bus(current_number_of_people):
-	# buses_needed_today = int( math.ceil( int(current_number_of_people) / 47.0)
-	print "Since 47 people can fit on a bus:"
-	print "We currently have %d people." % current_number_of_people
-	#, and we need %d buses." % (current_number_of_people, buses_needed_today)
+	buses_needed_today = int( math.ceil( int(current_number_of_people) / 47.0))
+	print "We currently have %d people, and we need %d buses." % (current_number_of_people, buses_needed_today)
+	# print "We need %d buses." % 
 
-print "Hello! I'm a script designed to calculate how many buses "
+print "Hello! I'm a script designed to calculate how many 47-passenger buses we need each day."
+print "\nOn Monday I use the number that you typed in on the comand line."
 people_per_bus(number_of_people)
