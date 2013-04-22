8da11f69c99b617bcaf037f5e1819d611f375d0f
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-22 13:52:38 -0400

    Renamed the function

--- ex19-ec03.py
+++ ex19-ec03.py
@@ -4,11 +4,14 @@ from sys import argv
 script, initial_number_of_people = argv
 number_of_people = int(initial_number_of_people)
 
-def people_per_bus(current_number_of_people):
+def bus_calculator(current_number_of_people):
 	buses_needed_today = int( math.ceil( int(current_number_of_people) / 47.0))
 	print "We currently have %d people, and we need %d buses." % (current_number_of_people, buses_needed_today)
-	# print "We need %d buses." % 
 
 print "Hello! I'm a script designed to calculate how many 47-passenger buses we need each day."
 print "\nOn Monday I use the number that you typed in on the comand line."
-people_per_bus(number_of_people)
+bus_calculator(number_of_people)
+
+print "\nOn Tuesday, an extra 57 people want to ride the bus."
+number_of_people = number_of_people + 57
+bus_calculator(number_of_people)
