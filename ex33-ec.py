6ca509a56dbdc3d4991a75a6ca68301888c3c2ff
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-24 17:35:47 -0400

    Correction made to meet second requirement of Extra Credit 1

--- ex33-ec.py
+++ ex33-ec.py
@@ -1,9 +1,10 @@
 # Modified for Exercise 33 Extra Credit: Various modifications as specified by the Extra Credit
 def loop_function():
 	i = 0
+	top_number = 6
 	numbers = []
 
-	while i < 6:
+	while i < top_number:
 		print "At the top i is %d" % i
 		numbers.append(i)
 
