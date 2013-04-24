f544e6696efdf6463deccd1fbb9bbbf92ef270e8
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-24 17:42:12 -0400

    Exercise 33 completed through Extra Credit 2

--- ex33-ec.py
+++ ex33-ec.py
@@ -1,7 +1,6 @@
 # Modified for Exercise 33 Extra Credit: Various modifications as specified by the Extra Credit
-def loop_function():
+def loop_function(top_number):
 	i = 0
-	top_number = 6
 	numbers = []
 
 	while i < top_number:
@@ -18,4 +17,11 @@ def loop_function():
 	for num in numbers:
 		print num
 	
-loop_function()
+print "\nDoing my loop function for 8 elements:"
+loop_function(8)
+
+print "\nNow doing the loop function for 5 elements:"
+loop_function(5)
+
+print "\nLastly, doing my loop function for 12 elements:"
+loop_function(12)
