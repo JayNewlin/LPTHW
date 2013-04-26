4e67e8ff318ba0148e0bb2e81744d4a9816ee2f4
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-26 10:00:41 -0400

    Start of entryway logic

--- ex36.py
+++ ex36.py
@@ -1,11 +1,23 @@
 # Game Project for Exercise 36
 def start():			# Put global variables and other initializing stuff here.
+	global has_gun
+	global has_cd
+	global has_cure
+	global has_gold
+	has_gun = False
+	has_cd = False
+	has_cure = False
+	has_gold = False
 	print "You are standing outside a weird mansion on the edge of town."
 	print "The front door stands ajar, so you step inside. (Perhaps not one of the bext decisions in your life.)"
 	entryway()
 
 def entryway():			# The entrance to the mansion. Game "really" starts here.
-	print "You made it into the entryway."
+	print "\nYou are standing in the entry hallway."
+	print "There is a door to your left and a door to your right."
+	print "Straight ahead of you is an ornate table with a huge vase full of dead flowers."
+	entryway_action = raw_input("What do you do?")
+	print entryway_action
 
 def snake_pit():		# Snakes live here
 	pass
