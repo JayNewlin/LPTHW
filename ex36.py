1e1695717ac93bf4e782940509a3d3faad06ffbe
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-26 09:32:16 -0400

    Started skeleton of Project 36

---
+++ ex36.py
@@ -0,0 +1,27 @@
+# Game Project for Exercise 36
+def start():			# Put global variables and other initializing stuff here.
+	pass
+
+def entryway():			# The entrance to the mansion. Game "really" starts here.
+	pass
+
+def snake_pit():		# Snakes live here
+	pass
+
+def ballroom():			# The Crazy DJ hangs out here.
+	pass
+
+def bar_room():			# The bartender is here.
+	pass
+
+def closet():			# The ghost lives in the Creepy Closet.
+	pass
+
+def treasury():			# There are 1000 gold pieces on a table here with a sign "Take some!"
+	pass
+
+def outside():			# Outside = Freedom! = one of the win states
+	pass
+
+
+start()
