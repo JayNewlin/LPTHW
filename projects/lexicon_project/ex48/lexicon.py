f71fc33b094989be7fe9515583b558937b6cc0d9
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-30 17:00:09 -0400

    Ex 48: Created word-type-sets and can find a word in them.

--- projects/lexicon_project/ex48/lexicon.py
+++ projects/lexicon_project/ex48/lexicon.py
@@ -9,3 +9,28 @@ def sentence_breaker(input_string):
 	words = input_string.split()
 	return words
 
+# Establish the word-types as sets
+directions = {'north', 'south', 'east', 'west', 'down',
+			  'up', 'left', 'right', 'back'}
+verbs = {'go', 'kill', 'eat', 'stop', 'shoot'}
+stop_words = {'the', 'in', 'of', 'from', 'at', 'it', 'to'}
+nouns = {'bear', 'princess', 'door', 'cabinet'}
+
+while True:
+	test_word = raw_input("What word shall I look up? ")
+
+	if test_word in directions:
+		print test_word, "is a direction."
+
+	elif test_word in verbs:
+		print "You can't fool me! %s is an action word; that means it's a verb!" % test_word
+
+	elif test_word in stop_words:
+		print "That's a stop word."
+
+	elif test_word in nouns:
+		print "I recognize %s as a noun." % test_word
+
+	else:
+		print "Sorry, but I haven't learned that word yet. :-("
+			
