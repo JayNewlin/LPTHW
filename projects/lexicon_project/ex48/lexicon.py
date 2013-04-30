bfed28273928529769f7cf58874dc83825730df0
Jay R. Newlin <jaynewlin@gmail.com>
2013-04-30 16:33:12 -0400

    Ex 48: convert_number and sentence_breaker work

--- projects/lexicon_project/ex48/lexicon.py
+++ projects/lexicon_project/ex48/lexicon.py
@@ -1,1 +1,11 @@
 # Created for Learning Python the Hard Way, Exercise 48
+def convert_number(input_string):					# From LPTHW, pg. 145
+	try:
+		return int(input_string)
+	except ValueError:
+		return None
+
+def sentence_breaker(input_string):
+	words = input_string.split()
+	return words
+
