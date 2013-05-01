09b48c0b4c35ae87f57516ec0e46e0feca264cac
Jay R. Newlin <jaynewlin@gmail.com>
2013-05-01 13:31:07 -0400

    Environment adjusted for start of Exercise 49

---
+++ projects/lexicon_project/ex49/ex49_learning.py
@@ -0,0 +1,2 @@
+# Created to test and review code segments mentioned in Exercise 49
+from ex49.lexicon49 import scan
--- projects/lexicon_project/setup.py
+++ projects/lexicon_project/setup.py
@@ -4,16 +4,16 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'Learning Python the Hard Way, Exercise 48 Extra Credits',
+	'description': 'Learning Python the Hard Way, Exercises 48 and 49',
 	'author': 'Jay R. Newlin',
 	'url': 'Jays-MacBook-Pro:~/PythonTheHardWay/projects/lexicon_project/',
 	'download_url': '',
 	'author_email': 'jay@dmgctrl.com',
-	'version': '0.5',
+	'version': '1.01',
 	'install_requires': ['nose'],
-	'packages': ['ex48'],
+	'packages': ['ex48', 'ex49'],
 	'scripts': [],
-	'name': 'Ex48_Lexicon_Project_EC'
+	'name': 'LPTHW_Ex48-49_Lexicon'
 }
 
 setup(**config)
