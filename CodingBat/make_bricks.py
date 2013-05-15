b830752b4b77fe84e507f091c94dc3f610fcc8c0
Jay R. Newlin <jaynewlin@gmail.com>
2013-05-15 09:46:19 -0400

    make_bricks complete

--- CodingBat/make_bricks.py
+++ CodingBat/make_bricks.py
@@ -1,10 +1,13 @@
 def make_bricks(small, big, goal):
-    if goal % 5 == 0:
+    if small + big * 5 < goal:
+        return False
 
-        if goal / 5 >= big:
+    elif goal % 5 == 0:
+
+        if goal / 5 <= big:
             return True
         else:
-            return "Need more math!"
+            return (big * 5 + small >= goal)
 
     else:
 
@@ -18,3 +21,9 @@ print "make_bricks(3, 1, 9) yields ", make_bricks(3, 1, 9)
 print "make_bricks(3, 2, 10) yields ", make_bricks(3, 2, 10)
 print "make_bricks(2, 2, 7) yields ", make_bricks(2, 2, 7)
 print "make_bricks(2, 2, 8) yields ", make_bricks(2, 2, 8)
+print "make_bricks(2, 2, 12) yields ", make_bricks(2, 2, 12)
+print "make_bricks(0, 2, 12) yields ", make_bricks(0, 2, 12)
+print "make_bricks(4, 1, 6) yields ", make_bricks(4, 1, 6)
+print "make_bricks(5, 4, 25) yields ", make_bricks(5, 4, 25)
+print "make_bricks(5, 4, 19) yields ", make_bricks(5, 4, 19)
+print "make_bricks(15, 1, 20) yields ", make_bricks(15, 1, 20)
