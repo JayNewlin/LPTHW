fe1dc4a2c31a4e448c33693de06fa07c2d6c3cdc
Jay R. Newlin <jaynewlin@gmail.com>
2013-05-20 16:46:38 -0400

    Completed "JavaScript Foundations: Arrays" stage

---
+++ websites/JavaScript/Foundations/Arrays/arrays.js
@@ -0,0 +1,120 @@
+// JavaScript Foundations: Arrays
+
+// var x = ['some', 'extra', 'words', 'here'];
+// //console.log(x.length);
+
+// var y = ['a string', 3, ['a sub array', 2 ,3, 4], 'treehouse', function(a, b){return a + b;}];
+
+// var z = [];
+
+// var a = new Array (50);
+
+// Getting and Setting
+
+// var my_array = ["Hello", 42, true, function (a) {return a * 2}];
+
+// var word = my_array[0];
+// var answer = my_array[1];
+// var doubler = my_array[3];
+// //console.log(doubler(10));
+
+// my_array[1] = 144;
+// var new_answer = my_array[1];
+
+// my_array[my_array.length] = "A new string"
+
+// Methods
+
+// var my_array = [2, 3, 4];
+// console.log(my_array.toString());
+
+// my_array.push(5);
+// console.log(my_array.toString());
+
+// var value = my_array.pop();
+// console.log(my_array.toString());
+
+// var value2 = my_array.pop();
+// console.log(my_array.toString());
+
+
+// var my_array = [2, 3, 4];
+// console.log(my_array.toString());
+
+// my_array.unshift(1);
+// console.log(my_array.toString());
+
+// value = my_array.shift();
+// console.log(my_array.toString());
+
+// var my_array = [10, 44, 32, 100, 0, 44, 3, 4];
+// console.log(my_array.toString());
+
+// my_array.sort(function(a, b) {
+
+// //	return a - b;
+// 	return Math.random() - 0.5;
+// });
+// console.log(my_array.toString());
+
+
+// var my_array = [1, 2, 3, 4, 5];
+// console.log(my_array.toString());
+
+// my_array.reverse();
+// console.log(my_array.toString());
+
+// var x = [1, 2, 3],
+// 	y = [4, 5, 6]
+// 	z = x.concat(y);
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// var my_array = [0, 1, 2, 3, 4, 5],
+// 	result = my_array.slice(1, 4);
+
+// console.log(result);
+
+// var words = ["these", "are", "some", "words"];
+// 	result = words.join(" ");
+
+// console.log(result);
+
+
+var my_array = [0, 1, 2, 3, 4, 5, 6];
+
+console.log(my_array.toString());
+
+my_array.splice(2, 1, 'two');
+
+console.log(my_array.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
