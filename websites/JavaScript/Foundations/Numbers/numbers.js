8a06e8069ae9a7396d2bb3fad88f9db33f26407e
Jay R. Newlin <jaynewlin@gmail.com>
2013-05-20 14:17:21 -0400

    Completed "JavaScript Foundations: Numbers" stage

---
+++ websites/JavaScript/Foundations/Numbers/numbers.js
@@ -0,0 +1,44 @@
+// JavaScript Numbers
+
+
+// Whole Numbers
+var a = 11,
+	b = -123;
+
+var c = 1.5,
+	d = 123.456789;
+
+var e = 0.1,
+	f = 0.2;
+
+var result = e * f;
+
+var g = 1.23E6;
+
+var h = 012;
+
+var i = 0xff;
+
+var j = parseInt("197", 10);
+
+var l = parseInt("There are 23 people", 10);
+
+var m = parseFloat("123.456 is a strange number")
+
+var o = 1 + 2;
+var p = 10- 7;
+
+var q = 3 * 4.2;
+var r = 15 / 10;
+
+var s = 15 %  10;
+
+var t = (1 + 2) * (3 / 4);
+
+// console.log(1 === 1);
+// console.log("2" == 2);
+// console.log(3 === parseInt("3", 10));
+// console.log(1 !== 3);
+// console.log(2 != "4");
+
+var u = Math.round(Math.random() * 10);
