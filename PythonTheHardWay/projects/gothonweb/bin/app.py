b5dfaa980f5c34dd50d7afd9ba5e66d9b37184ab
Jay R. Newlin <jaynewlin@gmail.com>
2013-05-23 10:41:09 -0400

    Ex 50 changed for "render.foo()"

--- PythonTheHardWay/projects/gothonweb/bin/app.py
+++ PythonTheHardWay/projects/gothonweb/bin/app.py
@@ -10,8 +10,8 @@ render = web.template.render('templates/')
 
 class index:
 	def GET(self):
-		greeting = "Hello World"
-		return render.index(greeting = greeting)
+		construction = "The Gothons are coming!!"
+		return render.foo(construction = construction)
 
 if __name__ == "__main__":
 	app.run()
