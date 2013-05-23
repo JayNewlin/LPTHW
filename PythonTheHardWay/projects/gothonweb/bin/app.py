5300eb189c43a9d598bded03bfa5234717a221ab
Jay R. Newlin <jaynewlin@gmail.com>
2013-05-23 11:44:20 -0400

    Ex51: Added Link back to Entry Form

--- PythonTheHardWay/projects/gothonweb/bin/app.py
+++ PythonTheHardWay/projects/gothonweb/bin/app.py
@@ -10,13 +10,12 @@ render = web.template.render('templates/')
 
 class Index(object):
 	def GET(self):
-		form = web.input(greet=None, name="Nobody")
+		return render.hello_form()
 
-		if form.greet:
-			greeting = "%s, %s" % (form.greet, form.name)
-			return render.index(greeting = greeting)
-		else:
-			return "ERROR: greet is required."
+	def POST(self):
+		form = web.input(name="Nobody", greet="Hello")
+		greeting = "%s, %s!" % (form.greet, form.name)
+		return render.index(greeting = greeting)
 
 if __name__ == "__main__":
 	app.run()
