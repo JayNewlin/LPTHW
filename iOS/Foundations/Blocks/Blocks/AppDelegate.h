abe0a77060e15d0b0f0f94f16da0e0f1270a31cf
Jay R. Newlin <jaynewlin@gmail.com>
2013-07-15 14:14:35 -0400

    Completed "Blocks pt. 1"

---
+++ iOS/Foundations/Blocks/Blocks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blocks
+//
+//  Created by Jay R Newlin on 7/15/13.
+//  Copyright (c) 2013 DmgCtrl Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/Foundations/Blocks/Blocks/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Blocks
+//
+//  Created by Jay R Newlin on 7/15/13.
+//  Copyright (c) 2013 DmgCtrl Learning. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
