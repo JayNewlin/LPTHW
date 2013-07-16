bb335a9e4e73a2054259af6e4309edf3b4f0d38f
Jay R. Newlin <jaynewlin@gmail.com>
2013-07-16 16:58:53 -0400

    Completed "Arrays" and Code Challenge

--- iOS/Foundations/CrystalBall/CrystalBall/ViewController.m
+++ iOS/Foundations/CrystalBall/CrystalBall/ViewController.m
@@ -60,10 +60,17 @@
 
 
 - (IBAction)buttonPressed:(UIButton *)sender {
-  NSString *myString = @"My name is Jay";
-  [myString length];
-  [myString uppercaseString];
+  NSArray *predictionArray = [[NSArray alloc] initWithObjects:@"It is certain",
+                     @"It is decidedly so",
+                     @"All signs say YES",
+                     @"The stars are not aligned",
+                     @"My reply is no",
+                     @"It is doubtful",
+                     @"Better not tell you now",
+                     @"Concentrate, and ask again",
+                     @"Unable to answer now",
+                     nil];
   
-  self.predictionLabel.text = @"Definitely yes";
+    self.predictionLabel.text = [predictionArray objectAtIndex:8];
 }
 @end
