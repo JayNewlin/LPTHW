372080f8a8e34bf88b142b97a41d995e0ea0c77e
Jay R. Newlin <jaynewlin@gmail.com>
2013-07-17 17:33:18 -0400

    Started "Building from Scratch"

---
+++ iOS/Foundations/BlogReader.fromTemplate/BlogReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Jay R Newlin on 7/17/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
