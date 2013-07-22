8736f046ed015a629d6dab2da401870914452b0e
Jay R. Newlin <jaynewlin@gmail.com>
2013-07-22 09:38:36 -0400

    Added Nav Controller and View Controller, prepping to display blog posts as web data on the new view

---
+++ iOS/Foundations/BlogReader/BlogReader/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  BlogReader
+//
+//  Created by Jay R Newlin on 7/22/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
