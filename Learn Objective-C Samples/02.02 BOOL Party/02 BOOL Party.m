4994b5e4d2e3d2b76c75a3992b94ff1dcb263124
Jay R. Newlin <jaynewlin@gmail.com>
2013-08-09 10:25:19 -0400

    Completed Chapter 2 of Learn Objective-C

--- Learn Objective-C Samples/02.02 BOOL Party/02 BOOL Party.m
+++ Learn Objective-C Samples/02.02 BOOL Party/02 BOOL Party.m
@@ -5,42 +5,37 @@
 
 BOOL areIntsDifferent (int thing1, int thing2)
 {
-    if (thing1 == thing2) {
-        return (NO);
-    } else {
-        return (YES);
-    }
-	
+  if (thing1 == thing2) {
+    return (NO);
+  }
+  else {
+    return (YES);
+  }
 } // areIntsDifferent
 
-
-// given a YES value, return the human-readable
-// string "YES". Otherwise return "NO"
+// given a NO value, return the human-readable
+// string "NO". Otherwise return "YES"
 
 NSString *boolString (BOOL yesNo)
 {
-    if (yesNo == NO) {
-        return (@"NO");
-    } else {
-        return (@"YES");
-    }
-	
+  if (yesNo == NO) {
+    return (@"NO");
+  } else {
+    return (@"YES");
+  }
 } // boolString
 
-
-int main (int argc, const char *argv[]) 
+int main (int argc, const char *argv[])
 {
-    BOOL areTheyDifferent;
-	
-    areTheyDifferent = areIntsDifferent (5, 5);
-	
-    NSLog (@"are %d and %d different? %@", 
-           5, 5, boolString(areTheyDifferent));
-	
-    areTheyDifferent = areIntsDifferent (23, 42);
-	
-    NSLog (@"are %d and %d different? %@", 
-           23, 42, boolString(areTheyDifferent));
+  BOOL areTheyDifferent;
+  
+  areTheyDifferent = areIntsDifferent(5, 5);
+  
+  NSLog(@"are %d and %d different? %@", 5, 5, boolString(areTheyDifferent));
+  
+  areTheyDifferent = areIntsDifferent(23, 42);
+  
+  NSLog(@"are %d and %d different? %@", 23, 42, boolString(areTheyDifferent));
 	
     return (0);
 	
