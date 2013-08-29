a1d9808e35835c002f95b61b7c9b1079c159e184
Jay R. Newlin <jaynewlin@gmail.com>
2013-08-29 10:32:51 -0400

    Restarted project

---
+++ iOS/Ribbit/Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Jay R Newlin on 8/29/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
