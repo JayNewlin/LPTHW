577ff089eb1f812ef692638867ca4209ab88dfeb
Jay R. Newlin <jaynewlin@gmail.com>
2013-09-03 10:01:16 -0400

    Added login view screens, navigation controllers, and appropriate segues

---
+++ iOS/Ribbit/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Ribbit
+//
+//  Created by Jay R Newlin on 9/3/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
---
+++ iOS/Ribbit/InboxViewController.m
@@ -0,0 +1,63 @@
+//
+//  InboxViewController.m
+//  Ribbit
+//
+//  Created by Jay R Newlin on 9/3/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import "InboxViewController.h"
+
+@interface InboxViewController ()
+
+@end
+
+@implementation InboxViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+  [self performSegueWithIdentifier:@"showLogin" sender:self];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
