5b851cef8ea8d9f2b8d5fcba703e5e17617a3112
Jay R. Newlin <jaynewlin@gmail.com>
2013-09-18 17:46:00 -0400

    Finished Treehouse "Into to Programming" Basics > Variables

---
+++ IntroProgramming/Basics/myscript.js
@@ -0,0 +1,11 @@
+console.log("Hello from myscript.js");
+console.log("Hello again!");
+
+var name = prompt("What is your name?");
+
+alert("Hello, " + name);
+
+name = "Fred";
+
+console.log("The user's name is " + name);
+
