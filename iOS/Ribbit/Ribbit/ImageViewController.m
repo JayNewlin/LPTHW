e1d0cfbee9f64ea4d264920a16eedf941e841a13
Jay R. Newlin <jaynewlin@gmail.com>
2013-09-18 15:31:10 -0400

    Added a timer of 10 secs to the Image Viewer

--- iOS/Ribbit/Ribbit/ImageViewController.m
+++ iOS/Ribbit/Ribbit/ImageViewController.m
@@ -21,9 +21,27 @@
   NSURL *imageFileUrl = [[NSURL alloc] initWithString:imageFile.url];
   NSData *imageData = [NSData dataWithContentsOfURL:imageFileUrl];
   self.imageView.image = [UIImage imageWithData:imageData];
+  
   NSString *senderName = [self.message objectForKey:@"senderName"];
   NSString *title = [NSString stringWithFormat:@"Sent from %@", senderName];
   self.navigationItem.title = title;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  
+  if ([self respondsToSelector:@selector(timeout)]) {
+    [NSTimer scheduledTimerWithTimeInterval:10 target:self selector:@selector(timeout) userInfo:nil repeats:NO];
+  }
+  else {
+    NSLog(@"Error: selector missing!");
+  }
+}
+
+#pragma mark - Helper methods
+
+- (void)timeout {
+  [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
