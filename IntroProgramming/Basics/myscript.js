d26a784cf027333c849ce2b768dbed4954a1883a
Jay R. Newlin <jaynewlin@gmail.com>
2013-09-19 10:32:01 -0400

    Finished "Comments" video

--- IntroProgramming/Basics/myscript.js
+++ IntroProgramming/Basics/myscript.js
@@ -1,11 +1,11 @@
+// Do a couple of console.log messages
 console.log("Hello from myscript.js");
-console.log("Hello again!");
+console.log("Hello again!"); // This is not needed
 
+/*
 var name = prompt("What is your name?");
-
 alert("Hello, " + name);
 
 name = "Fred";
-
 console.log("The user's name is " + name);
-
+*/
