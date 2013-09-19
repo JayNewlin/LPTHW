7dcd45f67de56d875bc77e49be3409397e98027b
Jay R. Newlin <jaynewlin@gmail.com>
2013-09-19 11:21:25 -0400

    Finished if/else

--- IntroProgramming/Basics/myscript.js
+++ IntroProgramming/Basics/myscript.js
@@ -2,10 +2,22 @@
 console.log("Hello from myscript.js");
 console.log("Hello again!"); // This is not needed
 
+
 /*
-var name = prompt("What is your name?");
 alert("Hello, " + name);
 
 name = "Fred";
 console.log("The user's name is " + name);
 */
+
+console.log("Before");
+
+var name = prompt("What is your name?");
+
+if (name) {
+  console.log("If block");
+} else {
+  console.log("Else block");
+}
+
+console.log("After");
