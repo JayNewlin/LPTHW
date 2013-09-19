356d8bbc69f5ab09e4c17764f52935690a9a016c
Jay R. Newlin <jaynewlin@gmail.com>
2013-09-19 12:08:56 -0400

    Completed "Arrays" video

--- IntroProgramming/Basics/myscript.js
+++ IntroProgramming/Basics/myscript.js
@@ -4,12 +4,14 @@ console.log("Hello again!"); // This is not needed
 
 
 /*
+var name = prompt("What is your name?");
 alert("Hello, " + name);
 
 name = "Fred";
 console.log("The user's name is " + name);
 */
 
+/*
 console.log("Before");
 
 var name = prompt("What is your name?");
@@ -21,3 +23,39 @@ if (name) {
 }
 
 console.log("After");
+*/
+
+/*
+console.log("Before");
+var counter = 10;
+
+while (counter) {
+  console.log("Hello world!");
+  counter = counter - 1;
+}
+
+while (prompt("What is your name?")) {
+  console.log("Got your name");
+}
+console.log("After");
+*/
+
+/*
+console.log("Before");
+for (var counter = 10; counter; counter = counter - 1) {
+  console.log("Hello world", counter);
+}
+
+console.log("After");
+*/
+
+var friends = ["Nick", "Michael", "Amit", "Allison", "John", "Rich"];
+console.log("These are my friends:", friends);
+console.log("I have", friends.length, "friends.");
+
+var friendNumber = 1;
+console.log("My friend at position", friendNumber, "is", friends[friendNumber]);
+
+for (var i = 0; i < friends.length; i+=1) {
+  console.log(friends[i]);
+};
