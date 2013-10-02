1e5bd8cd68b87f651c3c123c6bcc08e5b0264579
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-02 14:59:12 -0400

    Completed refactoring CarParts, using alloc/dealloc

--- Learn Objective-C Samples/10.01 CarParts-JRN/Car.m
+++ Learn Objective-C Samples/10.01 CarParts-JRN/Car.m
@@ -36,46 +36,48 @@
 
 - (void) setEngine: (Engine *) newEngine
 {
-    engine = newEngine;
+  [newEngine retain];
+  [engine release];
+  
+  engine = newEngine;
+  
 } // setEngine
 
 
 - (void) setTire: (Tire *) tire
          atIndex: (int) index
 {
-    if (index < 0 || index > 3) {
-        NSLog (@"bad index (%d) in setTire:atIndex:",
-               index);
-        exit (1);
-    }
-
-    tires[index] = tire;
-
+    [tires replaceObjectAtIndex:index
+                     withObject:tire];
+  
 } // setTire:atIndex:
 
 
 - (Tire *) tireAtIndex: (int) index
 {
-    if (index < 0 || index > 3) {
-        NSLog (@"bad index (%d) in tireAtIndex:",
-               index);
-        exit (1);
-    }
-
-    return (tires[index]);
+  Tire *tire;
+  tire = [tires objectAtIndex:index];
+  
+  return (tire);
 
 } // tireAtIndex:
 
-
+- (void) dealloc
+{
+  [tires release];
+  [engine release];
+  
+  [super dealloc];
+} // dealloc
 
 - (void) print
 {
-    NSLog (@"%@", engine);
-
-    NSLog (@"%@", tires[0]);
-    NSLog (@"%@", tires[1]);
-    NSLog (@"%@", tires[2]);
-    NSLog (@"%@", tires[3]);
+  int i;
+  for (i = 0; i < 4; i++) {
+    NSLog(@"%@", [self tireAtIndex:i]);
+  }
+  
+  NSLog(@"%@", engine);
 
 } // print
 
