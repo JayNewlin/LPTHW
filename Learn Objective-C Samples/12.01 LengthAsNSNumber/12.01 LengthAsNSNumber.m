94d76e3f27af95c79b4e2556f64860f02c8d4fd4
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-03 11:09:01 -0400

    Learning about Categories (Chapter 12), part 1

--- Learn Objective-C Samples/12.01 LengthAsNSNumber/12.01 LengthAsNSNumber.m
+++ Learn Objective-C Samples/12.01 LengthAsNSNumber/12.01 LengthAsNSNumber.m
@@ -23,25 +23,24 @@
 
 int main (int argc, const char *argv[])
 {
-	NSAutoreleasePool *pool;
-	pool = [[NSAutoreleasePool alloc] init];
+	@autoreleasepool {
 	
-	NSMutableDictionary *dict;
-	dict = [NSMutableDictionary dictionary];
+		NSMutableDictionary *dict;
+		dict = [NSMutableDictionary dictionary];
+		
+		[dict setObject: [@"hello" lengthAsNumber]
+				 forKey: @"hello"];
+		
+		[dict setObject: [@"iLikeFish" lengthAsNumber]
+				 forKey: @"iLikeFish"];
+		
+		[dict setObject: [@"Once upon a time" 
+						  lengthAsNumber]
+				 forKey: @"Once upon a time"];
+		
+		NSLog (@"%@", dict);
 	
-	[dict setObject: [@"hello" lengthAsNumber]
-			 forKey: @"hello"];
-	
-	[dict setObject: [@"iLikeFish" lengthAsNumber]
-			 forKey: @"iLikeFish"];
-	
-	[dict setObject: [@"Once upon a time" 
-					  lengthAsNumber]
-			 forKey: @"Once upon a time"];
-	
-	NSLog (@"%@", dict);
-	
-	[pool release];
+	}
 	
 	return (0);
 	
