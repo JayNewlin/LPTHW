8f4486ca57161713fda872a546fddf29c40d59b6
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-04 17:43:41 -0400

    Completed book, "Learning Objective-C on the Mac"

--- Learn Objective-C Samples/10.01 CarGaraging/06.01 CarParts-Split.m
+++ Learn Objective-C Samples/10.01 CarGaraging/06.01 CarParts-Split.m
@@ -38,30 +38,26 @@ int main (int argc, const char * argv[])
   
   car = makeCar (@"Herbie", @"Honda", @"CRX", 1984, 2, 110000, 58);
   [garage addCar: car];
-  car = makeCar (@"Badger", @"Acura", @"Integra", 1987, 5, 217036.7, 130);
-  [garage addCar: car];
-  car = makeCar (@"Elvis", @"Acura", @"Legend", 1989, 4, 28123.4, 151);
-  [garage addCar: car];
-  car = makeCar (@"Phoenix", @"Pontiac", @"Firebird", 1969, 2, 85128.3, 345);
-  [garage addCar: car];
-  car = makeCar (@"Streaker", @"Pontiac", @"Silver Streak", 1950, 2, 39100.0, 36);
-  [garage addCar: car];
-  car = makeCar (@"Judge", @"Pontiac", @"GTO", 1969, 2, 45132.2, 370);
-  [garage addCar: car];
-  car = makeCar (@"Paper Car", @"Plymouth", @"Valiant", 1965, 2, 76800, 105);
-  [garage addCar: car];
+//  Additional cars ignored for Chapter 17
+//  car = makeCar (@"Badger", @"Acura", @"Integra", 1987, 5, 217036.7, 130);
+//  [garage addCar: car];
+//  car = makeCar (@"Elvis", @"Acura", @"Legend", 1989, 4, 28123.4, 151);
+//  [garage addCar: car];
+//  car = makeCar (@"Phoenix", @"Pontiac", @"Firebird", 1969, 2, 85128.3, 345);
+//  [garage addCar: car];
+//  car = makeCar (@"Streaker", @"Pontiac", @"Silver Streak", 1950, 2, 39100.0, 36);
+//  [garage addCar: car];
+//  car = makeCar (@"Judge", @"Pontiac", @"GTO", 1969, 2, 45132.2, 370);
+//  [garage addCar: car];
+//  car = makeCar (@"Paper Car", @"Plymouth", @"Valiant", 1965, 2, 76800, 105);
+//  [garage addCar: car];
   
   [garage print];
   
-  NSNumber *count = [garage valueForKeyPath:@"cars.@count"];
-  NSLog(@"That's a total of %@ cars.", count);
+  NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name == 'Herbie'"];
   
-  NSNumber *sum = [garage valueForKeyPath:@"cars.@sum.mileage"];
-  NSLog(@"In total, they have traveled %@ miles. Wow! That's a lot of driving!", sum);
-  
-  NSNumber *avgMileage = [garage valueForKeyPath:@"cars.@avg.mileage"];
-  NSLog(@"Our cars average %@ miles driven.", avgMileage);
-  
+  BOOL match = [predicate evaluateWithObject:car];
+  NSLog(@"%s", (match) ? "YES" : "NO");
   return (0);
 	
 } // main
