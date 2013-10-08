debfdf8db1078c4122b73c4332c9fd01365d848f
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-08 15:46:56 -0400

    Completed Chapter 2 of Beginning iOS6

---
+++ Beginning iOS6 Dev/Hello World/Hello World/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Hello World
+//
+//  Created by Jay R Newlin on 10/8/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ Beginning iOS6 Dev/Hello World/Hello World/BIDViewController.m
@@ -0,0 +1,29 @@
+//
+//  BIDViewController.m
+//  Hello World
+//
+//  Created by Jay R Newlin on 10/8/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
