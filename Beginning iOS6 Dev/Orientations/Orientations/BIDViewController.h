e38007a605171cf620d884a752c5bf4fe3e1de27
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-09 16:01:50 -0400

    Orientations app (through p.121)

---
+++ Beginning iOS6 Dev/Orientations/Orientations/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Orientations
+//
+//  Created by Jay R Newlin on 10/9/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ Beginning iOS6 Dev/Orientations/Orientations/BIDViewController.m
@@ -0,0 +1,33 @@
+//
+//  BIDViewController.m
+//  Orientations
+//
+//  Created by Jay R Newlin on 10/9/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+  return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscapeLeft;
+}
+
+@end
