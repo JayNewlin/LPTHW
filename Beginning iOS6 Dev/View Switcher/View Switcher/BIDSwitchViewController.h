3237d5eb00f67b02cce3159041001da946672646
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-14 16:21:49 -0400

    Finished Modifying SwitchController.h

--- Beginning iOS6 Dev/View Switcher/View Switcher/BIDSwitchViewController.h
+++ Beginning iOS6 Dev/View Switcher/View Switcher/BIDSwitchViewController.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
+@class BIDYellowViewController;
+@class BIDBlueViewController;
+
 @interface BIDSwitchViewController : UIViewController
+
+@property (strong, nonatomic) BIDYellowViewController *yellowViewController;
+@property (strong, nonatomic) BIDBlueViewController *blueViewController;
+
+- (IBAction)switchViews:(id)sender;
 
 @end
