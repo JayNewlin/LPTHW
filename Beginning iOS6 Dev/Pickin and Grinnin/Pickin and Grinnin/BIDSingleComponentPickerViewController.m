c0a85061625634e7d01ebdfaa0d5428317bcf6e7
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-15 16:24:37 -0400

    Finished work on second Tab Bar Item, through pg.197

--- Beginning iOS6 Dev/Pickin and Grinnin/Pickin and Grinnin/BIDSingleComponentPickerViewController.m
+++ Beginning iOS6 Dev/Pickin and Grinnin/Pickin and Grinnin/BIDSingleComponentPickerViewController.m
@@ -27,12 +27,45 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+  self.characterNames = @[@"Picard", @"Riker", @"Data", @"Crusher", @"Troi", @"LeForge", @"Worf", @"Yar", @"O'Brien", @"Guinan"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPressed
+{
+  NSInteger row = [self.singlePicker selectedRowInComponent:0];
+  NSString *selected = self.characterNames[row];
+  NSString *title = [[NSString alloc] initWithFormat:@"You selected %@", selected];
+  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                  message:@"Thank you for choosing."
+                                                 delegate:nil
+                                        cancelButtonTitle:@"You're welcome!"
+                                        otherButtonTitles:nil];
+  [alert show];
+}
+
+#pragma mark - Picker Data Source Methods
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+  return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+  return [self.characterNames count];
+}
+
+#pragma mark Picker Delegate Methods
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+  return self.characterNames[row];
 }
 
 @end
