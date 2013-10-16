e0bfc60a55ea0a11e8ff5737bc07928d9a35c410
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-16 16:31:26 -0400

    Wired up and coded Dependent Component Picker, through pg. 208

--- Beginning iOS6 Dev/Pickin and Grinnin/Pickin and Grinnin/BIDDependentComponentPickerViewController.m
+++ Beginning iOS6 Dev/Pickin and Grinnin/Pickin and Grinnin/BIDDependentComponentPickerViewController.m
@@ -8,31 +8,96 @@
 
 #import "BIDDependentComponentPickerViewController.h"
 
-@interface BIDDependentComponentPickerViewController ()
-
-@end
+//@interface BIDDependentComponentPickerViewController ()
+//
+//@end
 
 @implementation BIDDependentComponentPickerViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+
+- (IBAction)buttonPressed
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+  NSInteger stateRow = [self.dependentPicker selectedRowInComponent:kStateComponent];
+  NSInteger zipRow = [self.dependentPicker selectedRowInComponent:kZipComponent];
+  NSString *state = self.states[stateRow];
+  NSString *zip = self.zips[zipRow];
+  
+  NSString *title = [[NSString alloc] initWithFormat:@"You selected ZIP Code %@.", zip];
+  NSString *message = [[NSString alloc] initWithFormat:@"%@ is in %@", zip, state];
+  
+  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                  message:message
+                                                 delegate:nil
+                                        cancelButtonTitle:@"OK"
+                                        otherButtonTitles:nil];
+  [alert show];
 }
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+  
+  NSBundle *bundle = [NSBundle mainBundle];
+  NSURL *plistURL = [bundle URLForResource:@"statedictionary" withExtension:@"plist"];
+  
+  self.stateZips = [NSDictionary dictionaryWithContentsOfURL:plistURL];
+  
+  NSArray *allStates = [self.stateZips allKeys];
+  NSArray *sortedStates = [allStates sortedArrayUsingSelector:@selector(compare:)];
+  self.states = sortedStates;
+  
+  NSString *selectedState = self.states[0];
+  self.zips = self.stateZips[selectedState];
+  
+  [self.dependentPicker reloadAllComponents];
+  
+//  NSString *logNote = [[NSString alloc] initWithFormat:@"I have %d at self.states and %d at self.zips", [self.states count], [self.zips count]];
+//  NSLog(logNote);
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Picker Data Source Methods
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+  if (component == kStateComponent) {
+    return [self.states count];
+  } else {
+    return [self.zips count];
+  }
+}
+
+#pragma mark Picker Delegate Methods
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+  if (component == kStateComponent) {
+    return self.states[row];
+  } else {
+    return self.zips[row];
+  }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+  if (component == kStateComponent) {
+    NSString *selectedState = self.states[row];
+    self.zips = self.stateZips[selectedState];
+    [self.dependentPicker reloadComponent:kZipComponent];
+    [self.dependentPicker selectRow:0 inComponent:kZipComponent animated:YES];
+  }
 }
 
 @end
