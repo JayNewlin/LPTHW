41dafc830b66056cc17b49e399fa3801cc6fbcc3
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-18 17:34:50 -0400

    Adjusted views/controls to use AutoLayout

--- iOS/Crystal Ball 7/Crystal Ball 7/THViewController.m
+++ iOS/Crystal Ball 7/Crystal Ball 7/THViewController.m
@@ -21,10 +21,6 @@
   
   self.crystalBall = [[THCrystalBall alloc] init];
   
-  UIImage *backgroundImage = [UIImage imageNamed:@"background"];
-  UIImageView *imageView = [[UIImageView alloc] initWithImage:backgroundImage];
-  [self.view insertSubview:imageView atIndex:0];
-  
 }
 
 - (void)didReceiveMemoryWarning
