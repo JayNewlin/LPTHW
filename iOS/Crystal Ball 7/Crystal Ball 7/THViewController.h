43b56ecd27eb808f25259fb506d5571fec64b350
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-21 14:59:27 -0400

    Added animation and adjusted look-and-feel

--- iOS/Crystal Ball 7/Crystal Ball 7/THViewController.h
+++ iOS/Crystal Ball 7/Crystal Ball 7/THViewController.h
@@ -14,6 +14,7 @@
 
 @property (strong, nonatomic) IBOutlet UILabel *predictionLabel;
 @property (strong, nonatomic) THCrystalBall *crystalBall;
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImageView;
 
 - (void) makePrediction;
 
--- iOS/Crystal Ball 7/Crystal Ball 7/THViewController.m
+++ iOS/Crystal Ball 7/Crystal Ball 7/THViewController.m
@@ -21,6 +21,70 @@
   
   self.crystalBall = [[THCrystalBall alloc] init];
   
+  self.backgroundImageView.animationImages = [[NSArray alloc] initWithObjects:
+                                              [UIImage imageNamed:@"CB00001"],
+                                              [UIImage imageNamed:@"CB00002"],
+                                              [UIImage imageNamed:@"CB00003"],
+                                              [UIImage imageNamed:@"CB00004"],
+                                              [UIImage imageNamed:@"CB00005"],
+                                              [UIImage imageNamed:@"CB00006"],
+                                              [UIImage imageNamed:@"CB00007"],
+                                              [UIImage imageNamed:@"CB00008"],
+                                              [UIImage imageNamed:@"CB00009"],
+                                              [UIImage imageNamed:@"CB00010"],
+                                              [UIImage imageNamed:@"CB00011"],
+                                              [UIImage imageNamed:@"CB00012"],
+                                              [UIImage imageNamed:@"CB00013"],
+                                              [UIImage imageNamed:@"CB00014"],
+                                              [UIImage imageNamed:@"CB00015"],
+                                              [UIImage imageNamed:@"CB00016"],
+                                              [UIImage imageNamed:@"CB00017"],
+                                              [UIImage imageNamed:@"CB00018"],
+                                              [UIImage imageNamed:@"CB00019"],
+                                              [UIImage imageNamed:@"CB00020"],
+                                              [UIImage imageNamed:@"CB00021"],
+                                              [UIImage imageNamed:@"CB00022"],
+                                              [UIImage imageNamed:@"CB00023"],
+                                              [UIImage imageNamed:@"CB00024"],
+                                              [UIImage imageNamed:@"CB00025"],
+                                              [UIImage imageNamed:@"CB00026"],
+                                              [UIImage imageNamed:@"CB00027"],
+                                              [UIImage imageNamed:@"CB00028"],
+                                              [UIImage imageNamed:@"CB00029"],
+                                              [UIImage imageNamed:@"CB00030"],
+                                              [UIImage imageNamed:@"CB00031"],
+                                              [UIImage imageNamed:@"CB00032"],
+                                              [UIImage imageNamed:@"CB00033"],
+                                              [UIImage imageNamed:@"CB00034"],
+                                              [UIImage imageNamed:@"CB00035"],
+                                              [UIImage imageNamed:@"CB00036"],
+                                              [UIImage imageNamed:@"CB00037"],
+                                              [UIImage imageNamed:@"CB00038"],
+                                              [UIImage imageNamed:@"CB00039"],
+                                              [UIImage imageNamed:@"CB00040"],
+                                              [UIImage imageNamed:@"CB00041"],
+                                              [UIImage imageNamed:@"CB00042"],
+                                              [UIImage imageNamed:@"CB00043"],
+                                              [UIImage imageNamed:@"CB00044"],
+                                              [UIImage imageNamed:@"CB00045"],
+                                              [UIImage imageNamed:@"CB00046"],
+                                              [UIImage imageNamed:@"CB00047"],
+                                              [UIImage imageNamed:@"CB00048"],
+                                              [UIImage imageNamed:@"CB00049"],
+                                              [UIImage imageNamed:@"CB00050"],
+                                              [UIImage imageNamed:@"CB00051"],
+                                              [UIImage imageNamed:@"CB00052"],
+                                              [UIImage imageNamed:@"CB00053"],
+                                              [UIImage imageNamed:@"CB00054"],
+                                              [UIImage imageNamed:@"CB00055"],
+                                              [UIImage imageNamed:@"CB00056"],
+                                              [UIImage imageNamed:@"CB00057"],
+                                              [UIImage imageNamed:@"CB00058"],
+                                              [UIImage imageNamed:@"CB00059"],
+                                              [UIImage imageNamed:@"CB00060"], nil];
+  self.backgroundImageView.animationDuration = 1.75f;
+  self.backgroundImageView.animationRepeatCount = 1;
+  
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,6 +96,12 @@
 #pragma mark - Prediction
 
 - (void) makePrediction {
+  [self.backgroundImageView startAnimating];
+  [self performSelector:@selector(predictionText) withObject:nil afterDelay:1.65];
+  
+}
+
+- (void) predictionText {
   self.predictionLabel.text = [self.crystalBall randomPrediction];
 }
 
