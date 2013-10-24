5739766ddaf778e717cf83b04a2ca91b38b31e17
Jay R. Newlin <jaynewlin@gmail.com>
2013-10-24 12:21:38 -0400

    Starting Sections project (pg. 252)

---
+++ Beginning iOS6 Dev/Learning Sections/Learning Sections/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Learning Sections
+//
+//  Created by Jay R Newlin on 10/24/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Beginning iOS6 Dev/Learning Sections/Learning SectionsTests/Learning_SectionsTests.m
@@ -0,0 +1,34 @@
+//
+//  Learning_SectionsTests.m
+//  Learning SectionsTests
+//
+//  Created by Jay R Newlin on 10/24/13.
+//  Copyright (c) 2013 DmgCtrl, Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Learning_SectionsTests : XCTestCase
+
+@end
+
+@implementation Learning_SectionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
